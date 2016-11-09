dec0080c1c927d32b55ce9d123649c06c6dc272d
byung82 <byung82@gmail.com>
2016-11-09 18:26:11 +0900

    카드 동기화 관련 수정

--- app/models/limit_card_log.rb
+++ app/models/limit_card_log.rb
@@ -13,7 +13,6 @@ class LimitCardLog < ActiveRecord::Base
         i_pprn1: pprn1,
         i_card_no: card_no,
         i_amt: amt,
-        i_card: card,
         i_pprn2: pprn2
     )[:o_id]
   end
