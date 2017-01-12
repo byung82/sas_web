56d07bbe1af894b31de8ffd119d4e88f10c86f0f
김병석 <byung82@gimbyeongseog-ui-MacBook-Pro.local>
2017-01-12 16:17:59 +0900

    total page 제거

--- app/serializers/api/v1/www/usages/index/usages_serializer.rb
+++ app/serializers/api/v1/www/usages/index/usages_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::Www::Usages::Index::UsagesSerializer < ActiveModel::Serializer
-  attributes :total_count, :total_page, :page
+  attributes :total_count, :page
 
   has_many :data, serializer: Api::V1::Www::Usages::Index::UsageSerializer
 end
