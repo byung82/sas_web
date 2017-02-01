a802c76d32edf183806f62f7a9a8c1edae96270a
김병석 <byung82@gimbyeongseog-ui-MacBook-Pro.local>
2017-02-01 14:30:09 +0900

    page 변경 수정

--- app/models/api/v1/response/usage.rb
+++ app/models/api/v1/response/usage.rb
@@ -16,14 +16,14 @@ module Api
           end
         end
 
-        def self.search(usage)
+        def self.search(usage, page)
           items = StoreLimitDet.where(business_no: usage.business_no)
               .where(user_seq: usage.user_seq)
               .where(status_cd: %w(LS002 LS003))
               # .page(1)
           #.page usage.page
 
-          items = items.page usage.page
+          items = items.page(page)
 
           usage = Usage.new
           usage.success = true
