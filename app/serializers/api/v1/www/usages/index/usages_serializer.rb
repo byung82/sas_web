94a6c36971d124d4707358ff6cb5137ab1ad212c
김병석 <byung82@gimbyeongseog-ui-MacBook-Pro.local>
2017-02-01 14:41:54 +0900

    데이터 버그 수정

--- app/serializers/api/v1/www/usages/index/usages_serializer.rb
+++ app/serializers/api/v1/www/usages/index/usages_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::Www::Usages::Index::UsagesSerializer < ActiveModel::Serializer
   attributes :total_count, :page
 
-  has_many :data, serializer: Api::V1::Www::Usages::Index::UsageSerializer
+  has_many :items, serializer: Api::V1::Www::Usages::Index::UsageSerializer
 end
