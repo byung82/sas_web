c1f52c14588fcd8cf3dfa9525d5180c5fe11ab59
김병석 <byung82@gimbyeongseog-ui-MacBook-Pro.local>
2017-04-10 11:14:30 +0900

    total page 제거

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -26,6 +26,8 @@ module Api
       def update
         item = Api::V1::Request::UpdateCard.create params
 
+        raise StandardError, '긴급 점검중입니다 잠시후에 진행해주세요'
+
         raise ActiveRecord::RecordInvalid, item if !item.valid?
 
         @item = StoreCard.find_by(card_no: item.card_no)
