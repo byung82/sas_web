19f580ccc7e3a3b76c33887b28b3134cb8d9f1c8
김병석 <byung82@gimbyeongseog-ui-MacBook-Pro.local>
2017-04-10 11:15:25 +0900

    total page 제거

--- app/controllers/api/v1/charges_controller.rb
+++ app/controllers/api/v1/charges_controller.rb
@@ -55,7 +55,11 @@ module Api
           raise StandardError, "오후 8시부터 오후 9시는 정기점검 시간입니다\n 오후 9시 이후에 충전 요청을 해주시기 바랍니다"
         end
 
-        raise StandardError, '긴급 점검중입니다 잠시후에 진행해주세요'
+        if current_user.login != 'lge'
+
+          raise StandardError, '긴급 점검중입니다 잠시후에 진행해주세요'
+
+        end
 
         # raise StandardError, '서버이전으로 충전이 필요하실 경우 연락을 주시기 바랍니다'
 
